ead4d7992699ce9f6aba78bb1751f811d2cee0fa
Precious Adebanjo <askpreshygold.070@gmail.com>
2023-09-09 04:22:24 +0100

    Changed branch

---
+++ main.py
@@ -0,0 +1,34 @@
+import pytz
+from datetime import datetime
+from fastapi import FastAPI, Query, HTTPException
+
+app = FastAPI()
+
+@app.get("/api")
+async def root(
+    slack_name: str = Query("Precious Damian", description="Slack Name"),
+    track: str = Query("Backend", description="Track"),
+):
+    # Getting current day of the week
+    current_day = datetime.now(pytz.timezone('Europe/Paris')).strftime('%A')
+
+    # Getting current UTC time
+    utc_time = datetime.utcnow().replace(tzinfo=pytz.utc).strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    # GitHub URL for the file being run
+    github_file_url = "https://github.com/username/repo/blob/main/file_name.ext"
+
+    # GitHub URL for the full source code repository
+    github_repo_url = "https://github.com/preciousdamian/hng-task-1-frontend.git"
+    
+    Response = {
+        "slack_name": slack_name,
+        "current_day": current_day,
+        "utc_time": utc_time,
+        "track": track,
+        "github_file_url": github_file_url,
+        "github_repo_url": github_repo_url,
+        "status_code": 200
+    }
+    
+    return Response
