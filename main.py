ef40bfa6dc188a8bb11952db8cd4c0d36ef4a412
Precious Adebanjo <askpreshygold.070@gmail.com>
2023-09-11 21:29:21 +0100

    Update port var

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import pytz
 from datetime import datetime
 from fastapi import FastAPI, Query
+import os
 
 app = FastAPI()
+
+port = int(os.getenv("PORT", 8000))
 
 @app.get("/api")
 async def root(slack_name: str = Query(default="", description="Slack Name"), track: str = Query(default="", description="Track")):
@@ -33,4 +36,4 @@ async def root(slack_name: str = Query(default="", description="Slack Name"), tr
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=port)
